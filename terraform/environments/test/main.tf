f27668c4d9ca82e186c279dd4444ce481f284b3f
rayan AL-anbari <rayann.alanbari@gmail.com>
2025-09-09 14:57:27 +0300

    Use Azure CLI auth for azurerm provider (remove client/secret)

--- terraform/environments/test/main.tf
+++ terraform/environments/test/main.tf
@@ -1,5 +1,4 @@
-provider "azurerm" {
-  tenant_id       = "${var.tenant_id}"
+provider "azurerm" {`n  features {}`n}"
   subscription_id = "${var.subscription_id}"
   client_id       = "${var.client_id}"
   client_secret   = "${var.client_secret}"
