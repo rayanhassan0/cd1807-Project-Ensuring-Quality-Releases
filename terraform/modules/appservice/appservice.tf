6bcb4e898c3106f572a3868183dac9a4654d231b
rayan AL-anbari <rayann.alanbari@gmail.com>
2025-09-09 21:49:41 +0300

    update pipeline and terraform configs for Udacity lab

--- terraform/modules/appservice/appservice.tf
+++ terraform/modules/appservice/appservice.tf
@@ -1,23 +1,18 @@
 resource "azurerm_service_plan" "test" {
-  name                = "${var.application_type}-${var.resource_type}"
-  location            = "${var.location}"
-  resource_group_name = "${var.resource_group}"
-
-  os_type  = "Linux"
-  sku_name = "S1"   # كان F1/B1 -> رفعناه إلى S1 لتفادي الكوتا
+  name                = "${var.application_type}-${var.resource_type}-plan"
+  location            = var.location
+  resource_group_name = var.resource_group
+  os_type             = "Linux"
+  sku_name            = "S1"
 }
 
 resource "azurerm_linux_web_app" "test" {
-  name                = "${var.application_type}-${var.resource_type}"
-  location            = "${var.location}"
-  resource_group_name = "${var.resource_group}"
+  name                = "${var.application_type}-${var.resource_type}-web"
+  location            = var.location
+  resource_group_name = var.resource_group
   service_plan_id     = azurerm_service_plan.test.id
 
-  app_settings = {
-    "WEBSITE_RUN_FROM_PACKAGE" = 0
-  }
-
   site_config {
-    always_on = false
+    always_on = true
   }
 }
