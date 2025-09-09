ad9ed695da3f6e2843b3175faf98af06ace6e49d
rayan AL-anbari <rayann.alanbari@gmail.com>
2025-09-09 13:55:31 +0300

    Remove embedded backend; rely on pipeline-generated backend.tf

--- terraform/environments/test/main.tf
+++ terraform/environments/test/main.tf
@@ -6,12 +6,7 @@ provider "azurerm" {
   features {}
 }
 terraform {
-  backend "azurerm" {
-    storage_account_name = ""
-    container_name       = ""
-    key                  = ""
-    access_key           = ""
-  }
+  
 }
 module "resource_group" {
   source               = "../../modules/resource_group"
