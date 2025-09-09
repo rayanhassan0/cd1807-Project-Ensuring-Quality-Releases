bd7cb36f29f253a81fff5cfceb95e857d18c77f6
rayan AL-anbari <rayann.alanbari@gmail.com>
2025-09-09 17:19:57 +0300

    Switch App Service plan from F1 (Free) to B1 to avoid quota error

--- terraform/modules/appservice/appservice.tf
+++ terraform/modules/appservice/appservice.tf
@@ -2,8 +2,10 @@ resource "azurerm_service_plan" "test" {
   name                = "${var.application_type}-${var.resource_type}"
   location            = "${var.location}"
   resource_group_name = "${var.resource_group}"
-  os_type             = "Linux"
-  sku_name            = "B1"   # <-- بدّلناها من F1 إلى B1 لأن F1 ما يدعم Linux
+
+  # بدّل من F1 (Free) إلى B1 (Basic) لتفادي مشكلة الكوتا
+  os_type  = "Linux"
+  sku_name = "B1"
 }
 
 resource "azurerm_linux_web_app" "test" {
@@ -13,10 +15,10 @@ resource "azurerm_linux_web_app" "test" {
   service_plan_id     = azurerm_service_plan.test.id
 
   app_settings = {
-    "WEBSITE_RUN_FROM_PACKAGE" = "0"  # app settings لازم تكون نصوص
+    "WEBSITE_RUN_FROM_PACKAGE" = 0
   }
 
   site_config {
-    always_on = false
+    always_on = false   # B1 يسمح بتعطيل AlwaysOn
   }
 }
