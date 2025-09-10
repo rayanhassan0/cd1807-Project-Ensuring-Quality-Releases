3b0e301186e7032d10ab4d232097be885e27fc11
rayan AL-anbari <rayann.alanbari@gmail.com>
2025-09-10 14:31:04 +0300

    chore: azure-pipelines + terraform backend + fixes

--- terraform/modules/appservice/appservice.tf
+++ terraform/modules/appservice/appservice.tf
@@ -1,18 +1,7 @@
 resource "azurerm_service_plan" "test" {
-  name                = "${var.application_type}-${var.resource_type}-plan"
+  name                = "${var.name}-AppService-plan"
+  resource_group_name = var.resource_group_name
   location            = var.location
-  resource_group_name = var.resource_group
-  os_type             = "Linux"
-  sku_name            = "S1"
-}
-
-resource "azurerm_linux_web_app" "test" {
-  name                = "${var.application_type}-${var.resource_type}-web"
-  location            = var.location
-  resource_group_name = var.resource_group
-  service_plan_id     = azurerm_service_plan.test.id
-
-  site_config {
-    always_on = true
-  }
+  os_type             = var.os_type
+  sku_name            = var.app_service_sku
 }
